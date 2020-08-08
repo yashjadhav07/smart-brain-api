e339bdac420514fa31d8201ee168aecf658a0320
Yash Jadhav <53169635+yashjadhav07@users.noreply.github.com>
2020-08-08 11:53:32 +0530

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai =require('clarifai') ;
+
+const app=new Clarifai.App({
+  apiKey:'954f3d2e6eea421db134b7532c92542f'
+});
+const handleApi=(req,res)=>{
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data=>{
+    res.json(data);
+  })
+  .catch(err=>res.status(400).json('unable to work with api'))
+}
+
+const handleImage=(req,res,db)=>{
+  const{id}=req.body;
+  db('users').where('id','=',id)
+  .increment('entries',1)
+  .returning('entries')
+  .then(entries=>{
+    res.json(entries[0]);
+  })
+  .catch(err=>res,status(400).json('unable to get entries'))
+}
+
+module.exports={
+  handleImage:handleImage,
+  handleApi:handleApi
+};
